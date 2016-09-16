990a1c27c9a1d05081b184e0820479f59c6735cc
jupono <jupono@ttu.ee>
2016-09-16 17:25:50 +0300

    EX02A

--- EX02A/EX02A.py
+++ EX02A/EX02A.py
@@ -1,6 +1,10 @@
+"""
+gffgdgdgdgdggfdgdgdfgg
+"""
+
 def profit(amount, days):
     income = amount
     for x in range(0, days):
-        income = income*1.01
+        income = income * 1.01
     return income - amount
 print(profit(100, 10))
