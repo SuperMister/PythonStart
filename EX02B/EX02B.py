02badbbacbe0a6eed78ce0ef63376c3eb54e7f40
jupono <jupono@ttu.ee>
2016-09-19 13:42:28 +0300

    EX02B

--- EX02B/EX02B.py
+++ EX02B/EX02B.py
@@ -7,9 +7,10 @@ Intressi arvestatakse igal kuul
 :return: kui palju raha (investeeringusumma + intressitulu) on võimalik etteantud aastate pärast tagasi saada
 """
 def investment(initial_amount, month_amount, inv_percent, years):
-    initial_amount = initial_amount * ((1 + (inv_percent/100))**years)
-    month_income = 0
-    for x in range (0, (years*12)-1):
-        month_income = (month_income + month_amount) * ((1 + ((inv_percent/100)/12)))
-    return month_income + initial_amount
-print(investment(1000, 100, 20, 1))
+    for x in range (0, (years*12)):
+        if(x == 0):
+            initial_amount = initial_amount * ((1 + ((inv_percent/100)/12)))
+        else:
+            initial_amount = (initial_amount + month_amount)  * ((1 + ((inv_percent / 100) / 12)))
+    return initial_amount
+print(investment(1000, 0, 20, 1))
