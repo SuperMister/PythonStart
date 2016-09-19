082800d366e178a439d9c50e6771c50346633b8f
jupono <jupono@ttu.ee>
2016-09-19 15:39:22 +0300

    EX02B

--- EX02B/EX02B.py
+++ EX02B/EX02B.py
@@ -1,16 +1,20 @@
-"""
-:param initial_amount: esmane investeeringu summa
-:param amount: igakuine investeeringu summa
-:param inv_percent: aastane intressi protsent
-:param years: investeerimisaastate arv
-Intressi arvestatakse igal kuul
-:return: kui palju raha (investeeringusumma + intressitulu) on võimalik etteantud aastate pärast tagasi saada
-"""
+"""EX02B."""
+
+
 def investment(initial_amount, month_amount, inv_percent, years):
-    for x in range (0, (years*12)):
+    """
+    Count intress amount.
+    :param initial_amount: esmane investeeringu summa
+    :param amount: igakuine investeeringu summa
+    :param inv_percent: aastane intressi protsent
+    :param years: investeerimisaastate arv(inglise keeles)kõik
+    :return: income
+    """
+    for x in range(0, (years*12)):
         if(x == 0):
             initial_amount = initial_amount * ((1 + ((inv_percent/100)/12)))
         else:
-            initial_amount = (initial_amount + month_amount)  * ((1 + ((inv_percent / 100) / 12)))
+            initial_amount = (initial_amount + month_amount) * \
+                             ((1 + ((inv_percent / 100) / 12)))
     return initial_amount
-print(investment(1000, 0, 20, 1))
+print(investment(1000, 20, 10, 5))
