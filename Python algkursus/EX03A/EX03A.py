d2305e42d0c47f94bccb39be625b7f6ed0a22901
jupono <jupono@ttu.ee>
2016-09-23 13:05:17 +0300

    EX03A

--- Python algkursus/EX03A/EX03A.py
+++ Python algkursus/EX03A/EX03A.py
@@ -3,16 +3,13 @@
 
 def fibonacci_sum(n):
     """
-    Please describe here the content of this function, parameters and returnable value.
     :param n:
     :return:
     """
-
-
     now = 1
     previous = 1
     sum = 0
-    for x in range (0, n+1):
+    for x in range(0, n+1):
         now = now + previous
         previous = now - previous
         sum = sum + now
