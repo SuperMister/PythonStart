7f250c250fc00fe9b9557cd66483086eb3468caf
jupono <jupono@ttu.ee>
2016-09-23 15:25:03 +0300

    EX03B

--- EX03B/EX03B.py
+++ EX03B/EX03B.py
@@ -1,5 +1,6 @@
 """Square root with different methods."""
 
+
 def square_root_with_newton_method(number, iterations):
     """
     :param number: Number we take root from.
@@ -7,7 +8,7 @@ def square_root_with_newton_method(number, iterations):
     :return: Return root with Newton method.
     """
     g = number / 2
-    for x in range (0, iterations):
+    for x in range(0, iterations):
         g = (g + number / g) / 2
     rounded_root1 = round(g, 3)
     return rounded_root1
@@ -15,23 +16,22 @@ def square_root_with_newton_method(number, iterations):
 
 def square_root_with_exp_ln(number):
     """
-    :param number:
-    :return:
+    :param number: Number we want root from.
+    :return: Return root with logarithm method.
    """
-    number = number**(1/2)
+    number **= (1 / 2)
     rounded_root2 = round(number, 3)
     return rounded_root2
 
+
 def square_root_result(number, iterations):
     """
-    Please describe here the content of this function, parameters and returnable value.
-    :param number:
-    :param iterations:
-    :return:
+    :param number: Number we take root from.
+    :param iterations: How many times we repeat action in first function.
+    :return: Return text with 2 answers.
     """
     newton_root = square_root_with_newton_method(number, iterations)
     normal_root = square_root_with_exp_ln(number)
-    print("Ruutjuur " + str(number) + "-st: " + "Newtoni meetod " + "(iter: " + str(iterations) + "): " + str(newton_root) +
-    "; exp-ln: " + str(normal_root))
+    print("Ruutjuur " + str(number) + "-st: " + "Newtoni meetod " + "(iter: " + str(iterations) + "): " + str(newton_root) + "; exp-ln: " + str(normal_root))
     return 0
-square_root_result(7, 2)
+square_root_result(99.69, 3)
