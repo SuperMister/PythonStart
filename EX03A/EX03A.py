7ca5fe6bf3c62a7161de272aa6ad9db0aef7e17a
jupono <jupono@ttu.ee>
2016-09-23 13:11:08 +0300

    EX03A

--- EX03A/EX03A.py
+++ EX03A/EX03A.py
@@ -1,19 +1,17 @@
-"""Fibonacci summary."""
+"""Fibonacci calcul."""
 
 
 def fibonacci_sum(n):
     """
-    :param n: last number in fibonacci
-    :return: summary of last number and all numbers before
+    :param n:
+    :return:
     """
-    if n == 0:
-        return 0
     now = 1
-    previous = 0
-    sum = 1
-    for x in range (0, n-1):
+    previous = 1
+    sum = 0
+    for x in range(0, n+1):
         now = now + previous
         previous = now - previous
         sum = sum + now
     return sum
-print(fibonacci_sum(0))
+print(fibonacci_sum(5))
