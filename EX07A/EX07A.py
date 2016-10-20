aecace984ad478f089a7a6b35be0b395bd819412
jupono <jupono@ttu.ee>
2016-10-20 20:41:57 +0300

    EX07A

--- EX07A/EX07A.py
+++ EX07A/EX07A.py
@@ -1,22 +1,24 @@
 def min_med_max(x, y, z):
-    if x > y > z:
-        return [z, y, x]
-    if x > z > y:
-        return [y, z, x]
-    if y > x > z:
-        return [z, x, y]
-    if y > z > x:
-        return [x, z, y]
-    if z > y > x:
-        return [x, y, z]
-    if z > x > y:
-        return [y, x, z]
-    if x == y == z:
-        return [x, y, z]
-    if x == y > z:
-        return [z, x, y]
-    if y == z > x:
-        return [x, z, y]
-    if x == z > y:
-        return [y, x , z]
-print(min_med_max(9, 6, 1))
+    """Compare 3 numbers and give maximum, minimum and median.
+
+    :param x: First number.
+    :param y: Second number.
+    :param z: Third number.
+    :return: Return list of numbers where first number is minimum, second is median and third is maximum
+    """
+    if x >= y and x >= z:
+        if y >= z:
+            return [z, y, x]
+        if z >= y:
+            return [y, z, x]
+    if y >= x and y >= z:
+        if x >= z:
+            return [z, x, y]
+        if z >= x:
+            return [x, z, y]
+    if z >= x and z >= y:
+        if x >= y:
+            return [y, x, z]
+        if y >= x:
+            return [x, y, z]
+print(min_med_max(7, 2, 2))
