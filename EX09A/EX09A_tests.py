e2b086b9754fcf74ba05ee459ce9bca04593c9fe
jupono <jupono@ttu.ee>
2016-11-10 17:29:01 +0200

    EX09A

--- EX09A/EX09A_tests.py
+++ EX09A/EX09A_tests.py
@@ -28,14 +28,9 @@ def test_the_longest_increase_not_none():
     assert EX09A.the_longest_increase_of_euro(["4.01.2016"], ["1.0898"]) is not None
 
 
-def test_no_syntax_error_in_increase():
-    """Testf if output is not SyntaxError."""
-    assert EX09A.read_from_file("1 EUR_X USD.txt") != SyntaxError
-
-
-def test_for_length_of_output():
-    """Test if function output has a minimum possible length."""
-    assert len(EX09A.the_longest_increase_of_euro(["4.01.2016"], ["1.0898"])) >= 37
+def test_the_longest_increase_is_not_syntax():
+    """Test if function output is not SyntaxError."""
+    assert EX09A.the_longest_increase_of_euro(["4.01.2016"], ["1.0898"]) != SyntaxError
 
 
 def test_type_of_output_of_main():
@@ -46,8 +41,3 @@ def test_type_of_output_of_main():
 def test_output():
     """Test if function output is right."""
     assert EX09A.euro_rates_main() == "The longest increase of euro in USD is: 22.04.2016 (1.1263) – 3.05.2016 (1.1569)"
-
-
-def test_main_output_length():
-    """Test if function output has a minimum possible length."""
-    assert len(EX09A.euro_rates_main()) >= 77
