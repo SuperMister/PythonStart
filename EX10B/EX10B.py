f1920d6af011cba5bc832714b21850b27c99e115
jupono <jupono@ttu.ee>
2016-11-16 17:29:06 +0200

    EX10B

--- EX10B/EX10B.py
+++ EX10B/EX10B.py
@@ -1,4 +1,26 @@
 """EX10B."""
 
+import random
 
 def main():
+    chars = []
+    for i in range(32, 256):
+        chars += chr(i)
+    return chars
+
+
+def random_string(chars, length):
+    word = ""
+    for i in range(length + 1):
+        word += random.choice(chars)
+    return word
+
+
+def check_string(text, pattern):
+    if pattern in text:
+        return True
+    return False
+
+
+def monkey_day(pattern):
+
