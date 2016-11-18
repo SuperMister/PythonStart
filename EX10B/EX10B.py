6011e805a673b5b042ba63ffd273cf2ec91b0ff2
jupono <jupono@ttu.ee>
2016-11-18 17:35:35 +0200

    EX10B

--- EX10B/EX10B.py
+++ EX10B/EX10B.py
@@ -1,26 +1,49 @@
 """EX10B."""
 
+
 import random
 
+
 def main():
-    chars = []
-    for i in range(32, 256):
-        chars += chr(i)
-    return chars
+    pattern, simulations, num_of_monkeys = get_inputs()
+    return monkey_day(pattern, num_of_monkeys, simulations)
 
 
 def random_string(chars, length):
     word = ""
-    for i in range(length + 1):
+    for i in range(length):
         word += random.choice(chars)
     return word
 
 
 def check_string(text, pattern):
-    if pattern in text:
-        return True
+    return pattern.lower() in text.lower()
+
+
+def monkey_day(pattern, num_of_monkeys, number=4 * 3600):
+    symbols = number
+    pattern = pattern.upper()
+    all_monkeys = [""] * num_of_monkeys
+    for x in range(symbols):
+        for i in range(len(all_monkeys)):
+            random_sign = chr(random.randrange(65, 91))
+            all_monkeys[i] += random_sign
+            if all_monkeys[i][-1] == pattern[len(all_monkeys[i]) - 1]:
+                if len(pattern) == len(all_monkeys[i]):
+                    return True
+            else:
+                if all_monkeys[i][-1] == pattern[0]:
+                    all_monkeys[i] = all_monkeys[i][-1]
+                else:
+                    all_monkeys[i] = str()
     return False
 
 
-def monkey_day(pattern):
-    pass
+def get_inputs():
+    word = input("Write your word: ")
+    simulations = int(input("Amount of simulations: "))
+    monkeys = int(input("Amount of monkeys: "))
+    return word, simulations, monkeys
+
+
+print(main())
