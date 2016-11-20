f236b74f3d0a2173e471e7446a63b10122c26926
jupono <jupono@ttu.ee>
2016-11-20 23:37:21 +0200

    EX11A

---
+++ EX11A/EX11A.py
@@ -0,0 +1,39 @@
+"""EX11A."""
+
+
+import random
+
+
+class Dissarange:
+    def __init__(self):
+        self.initial_list = []
+        self.result_list = []
+
+
+
+    def add_value(self, value):
+        return self.initial_list.append(value)
+
+
+    def new_order(self):
+        if len(self.initial_list) < 2:
+            self.result_list = self.initial_list
+            return self.result_list
+        else:
+            self.result_list = self.initial_list
+            for i in range(0, len(self.result_list) - 1):
+                random_index = random.randrange(0, len(self.result_list))
+                self.result_list[i] = self.result_list[random_index]
+
+    def get_list(self, feature):
+        if self.result_list == self.initial_list:
+            return []
+        else:
+            if feature == "initial":
+                return self.initial_list
+            if feature == "result":
+                return self.result_list
+
+
+
+
