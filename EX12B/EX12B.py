1af7870b02b8029663cf61d3678145b05ba19c5e
jupono <jupono@ttu.ee>
2016-12-02 17:15:30 +0200

    TOTAL

--- EX12B/EX12B.py
+++ EX12B/EX12B.py
@@ -1,32 +1,41 @@
-"""EX12B."""
+"""Dictionary of synonyms."""
 
 
 class Synonym:
-    def __init__(self, word_list):  # word_list on sünonüümide (samatähenduslike sõnade) nimekiri
+    """Class of synonyms."""
+
+    def __init__(self, word_list):
+        """Class constructor."""
         self.word_list = word_list
 
-    def list(self):  # tagastab nimekirjana (tähestiku järjekorras) kõik sünonüümi sõnad
+    def list(self):
+        """Return sorted list of words."""
         return sorted(self.word_list)
 
-    def add(self, word):  # word on sõna, mis lisatakse antud sünonüümile
+    def add(self, word):
+        """Add word to the list of words."""
         self.word_list.append(word)
 
-    def remove(self, word):  # word on sõna, mis eemaldatakse antud sünonüümist
+    def remove(self, word):
+        """Remove word from the list of words."""
         if word in self.word_list:
             self.word_list.remove(word)
 
 
 class SynonymDictionary:
-    def __init__(self, synonym_list):  # antakse sünonüümide (Synonym isendite) nimekiri
-        self.synonym_list = synonym_list
-        self.word = ""
-        self.list_syn = []
+    """Class of SynonymDictionary."""
 
-    def add(self, synonym):  # lisab Synonym isendi antud SynonymDictionary-sse
+    def __init__(self, synonym_list):
+        """Class constructor."""
+        self.synonym_list = synonym_list
+
+    def add(self, synonym):
+        """Add synonym to the list of synonyms."""
         if synonym not in self.synonym_list:
             self.synonym_list.append(synonym)
 
-    def find(self, word):  # tagastab kõik antud sõna sünonüümid (mitte sõna ennast) nimekirjana (tähestiku järjekorras)
+    def find(self, word):
+        """Find synonyms for the given word."""
         list_syn = []
         for i in self.synonym_list:
             if word in i.list():
@@ -34,7 +43,8 @@ class SynonymDictionary:
                 list_syn.remove(word)
         return list_syn
 
-    def list(self): # tagastab sünonüümid sõnastikuna(dict andmetüüp), kus sõna on võtmeks(key) ja selle sõna sünonüümide nimekiri(tähestiku järjekorras) on väärtuseks(value)
+    def list(self):
+        """Create dictionary with synonyms for every word."""
         synonyms = {}
         list_syn = []
         for i in self.synonym_list:
@@ -43,22 +53,3 @@ class SynonymDictionary:
                 list_syn.remove(x)
                 synonyms[x] = list_syn
         return synonyms
-
-
-
-syn1 = Synonym(['nimekiri', 'nimestik', 'loend'])
-syn2 = Synonym(['järjend', 'jada', 'sequence'])
-syn1.add('list')
-syn2.remove('jada')
-syn_dict = SynonymDictionary([syn1])
-syn_dict.add(syn2)
-syn_dict.find('loend')  # tagastab  ['list', 'nimekiri', 'nimestik']
-syn_dict.list()  # tagastab sõnastiku:
-# {
-# 'list':['loend', 'nimekiri', 'nimestik'],
-# 'loend':['list', 'nimekiri', 'nimestik'],
-# 'nimekiri':['list', 'loend', 'nimestik'],
-# 'nimestik':['list', 'loend', 'nimekiri'],
-# 'järjend':['sequence'],
-# 'sequence':['järjend']
-# }
