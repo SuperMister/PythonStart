70ad8767108fe1ae82330dd49eaa974d678de1fb
jupono <jupono@ttu.ee>
2016-12-13 16:15:01 +0200

    TOTAL

--- EX11A/EX11A.py
+++ EX11A/EX11A.py
@@ -22,9 +22,18 @@ class Disarrange:
 
         :return: Return disarranged list.
         """
+        """
+        if len(self.initial_list) % 2 == 0:
+            for i in range(0, 3):
+                self.result_list[i].append(self.initial_list[-i-1])
+                print(self.initial_list[-i-1])
+                print(self.initial_list[i])
+        """
+        for x in self.initial_list:
+            self.result_list.append(x)
         for i in range(len(self.initial_list) - 1):
-            self.initial_list[i], self.initial_list[i + 1] = self.initial_list[i + 1], self.initial_list[i]
-        self.result_list = self.initial_list
+            self.result_list[0] = self.initial_list[-1]
+            self.result_list[i + 1] = self.initial_list[i]
         return self.result_list
 
     def get_list(self, feature):
@@ -40,3 +49,15 @@ class Disarrange:
                 return self.initial_list
             if feature == "result":
                 return self.result_list
+
+a = Disarrange()
+a.add_value(1)
+a.add_value(2)
+a.add_value(3)
+a.add_value(4)
+a.add_value(5)
+a.add_value(6)
+print(a.get_list("initial"))
+a.new_order()
+print(a.get_list("result"))
+print(a.get_list("initial"))
