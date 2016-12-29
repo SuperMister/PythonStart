72bad3b89f95c193c87730b0a855b517c924700b
jupono <jupono@ttu.ee>
2016-12-29 12:36:56 +0200

    TOTAL

--- EX13B/EX13B.py
+++ EX13B/EX13B.py
@@ -1,9 +1,6 @@
 """EX13B."""
 
 
-import random
-
-
 class FindPair:
     """Class for finding pairs."""
 
@@ -11,8 +8,7 @@ class FindPair:
         """Class constructor."""
         self.file = file
         self.dict_groups = {}
-        self.biggest_group = []
-        self.all_pairs = []
+        self.big_groups = []
 
     def file_to_dict(self):
         """Convert information from file to the dictionary."""
@@ -20,6 +16,10 @@ class FindPair:
         person_group = []
         for i in y:
             person_group.append(i.rstrip("\n"))
+
+        if not person_group:
+            return "This file is empty, sorry!"
+
         for i in person_group:
             if i.split("\t")[1] in list(self.dict_groups.keys()):
                 self.dict_groups[i.split("\t")[1]].append(i.split("\t")[0])
@@ -27,47 +27,67 @@ class FindPair:
                 self.dict_groups[i.split("\t")[1]] = list([i.split("\t")[0]])
         return self.dict_groups
 
-    def find_pair(self):
-        """Find pair for every person in all groups."""
+    def control_for_small_groups(self):
+        """Control if there is only small groups in this file."""
         keys = list(self.dict_groups.keys())
-        group_remove = []
-        while True:
-            for y in keys:
-                if len(self.dict_groups[y]) < 3:
-                    for i in self.dict_groups[y]:
-                        b = random.choice(self.biggest_group)
-                        self.all_pairs.append(i + "\t " + b)
-                else:
-                    for j in self.dict_groups[y]:
-                        e = random.choice(self.dict_groups[y])
-                        if j != e:
-                            self.all_pairs.append(j + "\t" + e)
-                        else:
-                            for g in self.dict_groups[y]:
-                                group_remove.append(g)
-                            group_remove.remove(j)
-                            e = random.choice(group_remove)
-                            self.all_pairs.append(j + "\t" + e)
-            return self.all_pairs
+        not_small_groups = []
+        for i in keys:
+            if len(self.dict_groups[i]) > 3:
+                not_small_groups.append(self.dict_groups[i])
+        return not_small_groups
 
-    def find_biggest_group(self):
-        """Find the biggest group from all groups."""
-        self.biggest_group = []
+    def unite_small_into_big(self):
+        """Add small groups to the big ones."""
+        if not self.control_for_small_groups():
+            print("There is too many small groups!!!")
+            raise SystemExit
+        keys = list(self.dict_groups.keys())
+        for i in keys:
+            if len(self.dict_groups[i]) < 3:
+                for j in self.dict_groups[i]:
+                    self.big_groups[0].append(j)
+        return self.big_groups
+
+    def find_pair(self):
+        """Find pair for every person in his group."""
+        self.unite_small_into_big()
+        keys = list(self.dict_groups.keys())
+        for i in self.big_groups:
+            first_name = i[0]
+            for g in range(len(i) - 1):
+                i[g] = i[g] + "\t" + i[g + 1]
+            i[-1] = i[-1] + "\t" + first_name
+        return self.big_groups
+
+    def find_biggest_groups(self):
+        """Find the biggest group or groups from all groups."""
+        f_biggest_group = []
         for i in list(self.dict_groups.keys()):
-            if len(self.dict_groups[i]) > len(self.biggest_group):
-                self.biggest_group = self.dict_groups[i]
+            if len(self.dict_groups[i]) >= len(f_biggest_group):
+                if len(self.dict_groups[i]) > len(f_biggest_group):
+                    f_biggest_group = self.dict_groups[i]
 
-        return self.biggest_group
+        self.big_groups = []
+        for i in list(self.dict_groups.keys()):
+            if len(self.dict_groups[i]) == len(f_biggest_group):
+                self.big_groups.append(self.dict_groups[i])
+        return self.big_groups
 
     def print_output(self):
         """Print output."""
-        for i in self.all_pairs:
-            print(i)
+        for i in self.big_groups:
+            for g in i:
+                print(g)
 
 
-a = FindPair("grupid.txt")
+a = FindPair("grupid")
 a.file_to_dict()
-a.find_biggest_group()
-print(a.find_pair())
+a.find_biggest_groups()
+a.control_for_small_groups()
+print(a.unite_small_into_big())
+a.find_pair()
 a.print_output()
-print(list(a.dict_groups.keys()))
+
+
+
+
--- EX13B/EX13B_tests.py
+++ EX13B/EX13B_tests.py
@@ -2,30 +2,41 @@
 
 import EX13B
 
-a = EX13B.FindPair("grupid.txt")
+a = EX13B.FindPair("Test")
+b = EX13B.FindPair("Test for small")
+c = EX13B.FindPair("Blank")
 
 
 def test_file_to_dict():
-    """Tests."""
+    """Test if function is converting file to dictionary."""
+    assert c.file_to_dict() == "This file is empty, sorry!"
     assert isinstance(a.file_to_dict(), dict)
-    assert sorted(list(a.dict_groups.keys())) == sorted(['tпїЅdruk', 'vana naine', 'poiss', 'mees', 'vana mees', 'naine'])
+    assert isinstance(b.file_to_dict(), dict)
+    assert sorted(list(a.dict_groups.keys())) == sorted(['t?druk', 'vana naine', 'poiss', 'mees', 'vana mees', 'naine'])
 
 
 def test_biggest_group():
-    """Tests."""
-    assert isinstance(a.find_biggest_group(), list)
-    assert a.find_biggest_group() == ['Peeter', 'Ants', 'Juhan', 'Rudolf']
+    """Test if function is finding biggest group."""
+    assert isinstance(a.find_biggest_groups(), list)
+    assert a.find_biggest_groups() == [['Peeter', 'Ants', 'Juhan', 'Rudolf']]
 
 
 def test_find_pair():
-    """Tests."""
+    """Test if group are generating pairs that is not same."""
     for i in a.find_pair():
-        assert i.split("\t")[0] != i.split("\t")[1]
+        for j in i:
+            assert j.split("\t")[0] != j.split("\t")[1]
     assert isinstance(a.find_pair(), list)
-    for i in a.all_pairs:
-        a.all_pairs.count(i) == 1
+    for i in a.big_groups:
+        a.big_groups.count(i) == 1
+
+
+def test_control_for_small():
+    """Test if function is controlling file for small groups."""
+    assert b.control_for_small_groups() == []
+    assert a.control_for_small_groups() is not []
 
 
 def test_output():
-    """Tests."""
+    """Test if function is giving output."""
     assert a.print_output() is None
